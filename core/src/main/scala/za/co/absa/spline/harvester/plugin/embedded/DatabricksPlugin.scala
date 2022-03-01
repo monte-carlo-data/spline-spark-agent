8719456c227ef0430c1cce8fdeb15cbc54f5083f
Corey Fritz <cfritz@montecarlodata.com>
2022-02-28 22:13:24 -0500

    checkpoint (WIP)

--- core/src/main/scala/za/co/absa/spline/harvester/plugin/embedded/DatabricksPlugin.scala
+++ core/src/main/scala/za/co/absa/spline/harvester/plugin/embedded/DatabricksPlugin.scala
@@ -17,13 +17,17 @@
 package za.co.absa.spline.harvester.plugin.embedded
 
 import javax.annotation.Priority
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.catalog.CatalogTable
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.catalyst.plans.logical.{LogicalPlan, SubqueryAlias, View}
+import org.apache.spark.sql.execution.datasources.LogicalRelation
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import za.co.absa.commons.reflect.ReflectionUtils.extractFieldValue
 import za.co.absa.commons.reflect.extractors.SafeTypeMatchingExtractor
-import za.co.absa.spline.harvester.plugin.Plugin.{Precedence, WriteNodeInfo}
+import za.co.absa.spline.harvester.builder.SourceIdentifier
+import za.co.absa.spline.harvester.plugin.Plugin.{Params, Precedence, ReadNodeInfo, WriteNodeInfo}
 import za.co.absa.spline.harvester.plugin.embedded.DatabricksPlugin.`_: DataBricksCreateDeltaTableCommand`
+import za.co.absa.spline.harvester.plugin.embedded.DatabricksPlugin.`_: DataBricksMergeIntoCommand`
 import za.co.absa.spline.harvester.plugin.extractor.CatalogTableExtractor
 import za.co.absa.spline.harvester.plugin.{Plugin, ReadNodeProcessing, WriteNodeProcessing}
 import za.co.absa.spline.harvester.qualifier.PathQualifier
@@ -35,6 +39,7 @@ class DatabricksPlugin(
   pathQualifier: PathQualifier,
   session: SparkSession)
   extends Plugin
+    with Logging
     with WriteNodeProcessing {
 
   private val extractor = new CatalogTableExtractor(session.catalog, pathQualifier)
@@ -45,6 +50,37 @@ class DatabricksPlugin(
       val saveMode = extractFieldValue[SaveMode](command, "mode")
       val query = extractFieldValue[Option[LogicalPlan]](command, "query").get
       extractor.asTableWrite(table, saveMode, query)
+
+    case `_: DataBricksMergeIntoCommand`(command) =>
+      val target = extractFieldValue[LogicalPlan](command, "target")
+      logInfo(s"Merge target: ${target}")
+      target.children.foreach(child => {
+        logInfo(s"Merge target child: ${child}")
+        child.children.foreach(grandchild => {
+          logInfo(s"Merge target grandchild (type=${grandchild.getClass.getName}): ${grandchild}")
+          if (grandchild.isInstanceOf[LogicalRelation]) {
+            var relation = grandchild.asInstanceOf[LogicalRelation]
+            var table = relation.catalogTable
+            if (table.isDefined) {
+              logInfo(s"Merge target info: identifier=${table.get.identifier}, database=${table.get.identifier.database}, table=${table.get.identifier.table}, provider=${table.get.provider}, location=${table.get.storage.locationUri}")
+            }
+          }
+        })
+      })
+
+      val source = extractFieldValue[LogicalPlan](command, "source")
+      logInfo(s"Merge source: ${source}")
+      source.children.foreach(child => {
+        logInfo(s"Merge source child: ${child}")
+        child.children.foreach(grandchild => {
+          if (grandchild.isInstanceOf[View]) {
+            val view = grandchild.asInstanceOf[View]
+            logInfo(s"Merge source info: identifier=${view.desc.identifier.identifier}, database=${view.desc.identifier.database}, table=${view.desc.identifier.table}, provider=${view.desc.provider}, location=${view.desc.storage.locationUri}")
+          }
+        })
+      })
+
+      (SourceIdentifier(None, "foo"), SaveMode.Overwrite, target, Map("table" -> Map("identifier" -> "foo")))
   }
 }
 
@@ -52,4 +88,8 @@ object DatabricksPlugin {
 
   private object `_: DataBricksCreateDeltaTableCommand` extends SafeTypeMatchingExtractor[AnyRef](
     "com.databricks.sql.transaction.tahoe.commands.CreateDeltaTableCommand")
+
+  private object `_: DataBricksMergeIntoCommand` extends SafeTypeMatchingExtractor[AnyRef](
+    "com.databricks.sql.transaction.tahoe.commands.MergeIntoCommandEdge"
+  )
 }
